23bcf82dcfe3b7181989eb7505e62bb98ad697c4
SpatiumCattus <45818030+SpatiumCattus@users.noreply.github.com>
2021-06-19 12:35:32 +0300

    start

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+
+
+        <div id="navbar">
+            <nav class="navbar-logo">
+                <li>
+                    <img id="logo" src="img/logo.png" alt="logo">
+                </li>
+                <li>
+                    <p id="sc_name">Spatium Cattus</p>
+                </li>
+            </nav>
+            <nav class="navbar-menu">
+                <li>home</li>
+                <li>info</li>
+                <li></li>
+            </nav>
+        </div>
+
+
+    </header>
+
+</body>
+
+</html>
